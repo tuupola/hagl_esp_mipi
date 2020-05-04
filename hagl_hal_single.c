b92efda18574b9958c7c6ea5bfd87802b4e73aa8
Mika Tuupola <tuupola@appelsiini.net>
2020-05-04 11:18:03 +0700

    Split different buffering options to separate files (#6)
    Not sure if this is the correct way to dot this, but the #ifdef - #endif
    soup was impossible to read before.

---
+++ hagl_hal_single.c
@@ -0,0 +1,106 @@
+/*
+
+MIT License
+
+Copyright (c) 2019-2020 Mika Tuupola
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-cut-
+
+This file is part of the MIPI DCS HAL for HAGL graphics library:
+https://github.com/tuupola/hagl_esp_mipi/
+
+SPDX-License-Identifier: MIT
+
+*/
+
+#include "sdkconfig.h"
+
+#ifdef CONFIG_HAGL_HAL_NO_BUFFERING
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+#include <esp_log.h>
+#include <esp_heap_caps.h>
+#include <string.h>
+#include <mipi_display.h>
+#include <bitmap.h>
+#include <hagl.h>
+
+#include "hagl_hal.h"
+
+static spi_device_handle_t spi;
+static const char *TAG = "hagl_esp_mipi";
+
+/*
+ * Initializes the MIPI display
+ */
+bitmap_t *hagl_hal_init(void)
+{
+    mipi_display_init(&spi);
+    return NULL;
+}
+
+void hagl_hal_put_pixel(int16_t x0, int16_t y0, uint16_t color)
+{
+    mipi_display_write(spi, x0, y0, 1, 1, (uint8_t *) &color);
+}
+
+/*
+ * Blit the source bitmap the display
+ */
+void hagl_hal_blit(uint16_t x0, uint16_t y0, bitmap_t *src)
+{
+    mipi_display_write(spi, x0, y0, src->width, src->height, (uint8_t *) src->buffer);
+}
+
+/*
+ * Accelerated horizontal line drawing
+ */
+void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t width, uint16_t color)
+{
+    static uint16_t line[DISPLAY_WIDTH];
+    uint16_t *ptr = line;
+    uint16_t height = 1;
+
+    for (uint16_t x = 0; x < width; x++) {
+        *(ptr++) = color;
+    }
+
+    mipi_display_write(spi, x0, y0, width, height, (uint8_t *) line);
+}
+
+/*
+ * Accelerated vertical line drawing
+ */
+void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t height, uint16_t color)
+{
+    uint16_t line[DISPLAY_HEIGHT];
+    uint16_t *ptr = line;
+    uint16_t width = 1;
+
+    for (uint16_t x = 0; x < height; x++) {
+        *(ptr++) = color;
+    }
+
+    mipi_display_write(spi, x0, y0, width, height, (uint8_t *) line);
+}
+
+#endif /* CONFIG_HAGL_HAL_NO_BUFFERING */
