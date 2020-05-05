2c54c56cdc457a1899009342b513106fb84fbe1a
Mika Tuupola <tuupola@appelsiini.net>
2020-05-05 15:32:29 +0700

    Add unbuffered INDEX8 mode

--- hagl_hal.h
+++ hagl_hal.h
@@ -42,26 +42,26 @@ extern "C" {
 
 #include <stdint.h>
 #include <bitmap.h>
+#include <mipi_display.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-#define DISPLAY_WIDTH       (CONFIG_MIPI_DISPLAY_WIDTH)
-#define DISPLAY_HEIGHT      (CONFIG_MIPI_DISPLAY_HEIGHT)
-
-#ifdef  MIPI_DCS_PIXEL_FORMAT_8BIT_SELECTED
-#define DISPLAY_DEPTH       (8)
+#if defined (CONFIG_MIPI_DCS_PIXEL_FORMAT_8BIT_SELECTED)
+#define HAGL_HAL_FORMAT_INDEX8
+typedef uint8_t color_t;
+#elif defined (CONFIG_MIPI_DCS_PIXEL_FORMAT_8BIT_EMULATED_SELECTED)
+#define HAGL_HAL_FORMAT_INDEX8
 typedef uint8_t color_t;
 #else
-#define DISPLAY_DEPTH       (16)
 typedef uint16_t color_t;
 #endif
 
 /* This is the only mandatory function HAL must provide. */
-void hagl_hal_put_pixel(int16_t x0, int16_t y0, uint16_t color);
+void hagl_hal_put_pixel(int16_t x0, int16_t y0, color_t color);
 
-/* No buffering is the default. */
-#ifdef CONFIG_HAGL_HAL_NO_BUFFERING
+/* Unbuffered RGB565. */
+#if defined (CONFIG_HAGL_HAL_NO_BUFFERING) && !defined (HAGL_HAL_FORMAT_INDEX8)
 #define HAGL_HAS_HAL_INIT
 #define HAGL_HAS_HAL_BLIT
 #define HAGL_HAS_HAL_HLINE
@@ -69,9 +69,22 @@ void hagl_hal_put_pixel(int16_t x0, int16_t y0, uint16_t color);
 
 bitmap_t *hagl_hal_init(void);
 void hagl_hal_blit(uint16_t x0, uint16_t y0, bitmap_t *src);
-void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t w, uint16_t color);
-void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, uint16_t color);
-#endif /* CONFIG_HAGL_HAL_NO_BUFFERING */
+void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t w, color_t color);
+void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, color_t color);
+#endif
+
+/* Unbuffered INDEX8. */
+#if defined (CONFIG_HAGL_HAL_NO_BUFFERING) && defined (HAGL_HAL_FORMAT_INDEX8)
+#define HAGL_HAS_HAL_INIT
+#define HAGL_HAS_HAL_HLINE
+#define HAGL_HAS_HAL_VLINE
+
+bitmap_t *hagl_hal_init(void);
+void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t w, color_t color);
+void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, color_t color);
+#endif
+
+
 
 #ifdef CONFIG_HAGL_HAL_USE_TRIPLE_BUFFERING
 #define HAGL_HAL_USE_BUFFERING
@@ -86,8 +99,8 @@ void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, uint16_t color);
 bitmap_t *hagl_hal_init(void);
 void hagl_hal_blit(uint16_t x0, uint16_t y0, bitmap_t *src);
 void hagl_hal_scale_blit(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, bitmap_t *src);
-void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t w, uint16_t color);
-void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, uint16_t color);
+void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t w, color_t color);
+void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, color_t color);
 void hagl_hal_flush();
 #endif /* CONFIG_HAGL_HAL_USE_TRIPLE_BUFFERING */
 
@@ -95,17 +108,17 @@ void hagl_hal_flush();
 #define HAGL_HAL_USE_BUFFERING
 
 #define HAGL_HAS_HAL_INIT
-#define HAGL_HAS_HAL_BLIT
-#define HAGL_HAS_HAL_SCALE_BLIT
-#define HAGL_HAS_HAL_HLINE
-#define HAGL_HAS_HAL_VLINE
+// #define HAGL_HAS_HAL_BLIT
+// #define HAGL_HAS_HAL_SCALE_BLIT
+// #define HAGL_HAS_HAL_HLINE
+// #define HAGL_HAS_HAL_VLINE
 #define HAGL_HAS_HAL_FLUSH
 
 bitmap_t *hagl_hal_init(void);
 void hagl_hal_blit(uint16_t x0, uint16_t y0, bitmap_t *src);
 void hagl_hal_scale_blit(uint16_t x0, uint16_t y0, uint16_t w, uint16_t h, bitmap_t *src);
-void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t w, uint16_t color);
-void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, uint16_t color);
+void hagl_hal_hline(int16_t x0, int16_t y0, uint16_t w, color_t color);
+void hagl_hal_vline(int16_t x0, int16_t y0, uint16_t h, color_t color);
 void hagl_hal_flush();
 #endif /* CONFIG_HAGL_HAL_USE_DOUBLE_BUFFERING */
 
